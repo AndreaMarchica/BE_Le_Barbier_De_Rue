d46fbf4fb747dbc662818fa744f09a4265946cc6
AndreaMarchica <andrea.marchica94@gmail.com>
2024-02-05 11:33:11 +0100

    create project folder

---
+++ src/main/java/andreamarchica/BE_Le_Barbier_De_Rue/BeLeBarbierDeRueApplication.java
@@ -0,0 +1,13 @@
+package andreamarchica.BE_Le_Barbier_De_Rue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeLeBarbierDeRueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeLeBarbierDeRueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/andreamarchica/BE_Le_Barbier_De_Rue/BeLeBarbierDeRueApplicationTests.java
@@ -0,0 +1,13 @@
+package andreamarchica.BE_Le_Barbier_De_Rue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeLeBarbierDeRueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
