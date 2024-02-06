0f184bc7abc215d4aeae8caa128f9e31155b19ee
AndreaMarchica <andrea.marchica94@gmail.com>
2024-02-06 18:22:11 +0100

    adds reservation features like repository, authentication, controller and service

---
+++ src/main/java/andreamarchica/BE_Le_Barbier_De_Rue/repository/ReservationRepository.java
@@ -0,0 +1,11 @@
+package andreamarchica.BE_Le_Barbier_De_Rue.repository;
+
+import andreamarchica.BE_Le_Barbier_De_Rue.entities.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, UUID> {
+}
